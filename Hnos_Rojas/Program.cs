19494bbc46caa388fcc95c7616a1df78284375a0
Necrotavo <zelta_tavo1@hotmail.com>
2019-11-08 11:23:06 -0600

    Cambio de nombre del form

--- Hnos_Rojas/Program.cs
+++ Hnos_Rojas/Program.cs
@@ -16,7 +16,7 @@ namespace Hnos_Rojas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new VentanaPrincipal());
         }
     }
 }
