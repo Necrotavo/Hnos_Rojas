24f2ea6ac213079215566802ca69b627182c3300
KevinJag21 <kevin.araya@ucrso.info>
2019-11-08 13:39:45 -0600

    Reduccióm de tamaño

--- Hnos_Rojas/Form1.Designer.cs
+++ Hnos_Rojas/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(344, 205);
             this.Name = "VentanaPrincipal";
             this.Text = "Form1";
             this.ResumeLayout(false);
