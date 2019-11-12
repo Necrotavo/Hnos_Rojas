abced76eee8a66d667359037bb3c48352d27ddfd
Daniel Ruiz <ruizaraya14@gmail.com>
2019-11-12 11:06:44 -0600

    Creé los paquetes y la clase DO_Cliente

---
+++ ClassLibrary1/DO_Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class DO_Cliente
+    {
+        private String id;
+        private String nombre;
+        private String primerApellido;
+        private String segundoApellido;
+        private String direccion;
+        private int telefono;
+        private String estado;
+    }
+}
