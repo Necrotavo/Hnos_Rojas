1f0adfcbd7744ee671f840cdbbee42ec28db7cc0
AndreyCrandys <andrey.nunez@ucrso.info>
2019-11-12 14:30:43 -0600

    Creacion de clases en DO

--- ClassLibrary1/DO_Cliente.cs
+++ ClassLibrary1/DO_Cliente.cs
@@ -15,5 +15,18 @@ namespace DO
         private String direccion;
         private int telefono;
         private String estado;
+
+        public DO_Cliente() {
+        }
+
+        public DO_Cliente(String _id, String _nombre, String _primerApellido, String _segundoApellido, String _direccion, int _telefono, String _estado) {
+            this.id = _id;
+            this.nombre = _nombre;
+            this.primerApellido = _primerApellido;
+            this.segundoApellido = _segundoApellido;
+            this.direccion = _direccion;
+            this.telefono = _telefono;
+            this.estado = _estado;
+        }
     }
 }
