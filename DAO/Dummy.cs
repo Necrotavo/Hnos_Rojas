d50038ad651619df3b275aa0893b8121b470d521
JoJoLa <jonathan.arroyo@ucrso.info>
2019-11-12 14:46:19 -0600

    Dummy BuscarProducto

---
+++ DAO/Dummy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class Dummy
+    {
+
+        private List<String> lista_Productos;
+
+        /*("1",750,1000,1,"Leche", 3);
+         * ("2",100,250,1,"Tang", 10);
+         * ("3",500,850,1,"Mantequilla", 5);
+         */
+        public void BuscarProducto (String _codigo)
+        {
+
+        }
+
+
+    }
+}
