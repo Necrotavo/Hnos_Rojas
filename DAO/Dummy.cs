ea7482d9094db8b1d4c449aaa2b36b52cdb7dc77
JoJoLa <jonathan.arroyo@ucrso.info>
2019-11-12 15:00:57 -0600

    Dummy BuscarProducto_2

--- DAO/Dummy.cs
+++ DAO/Dummy.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DO;
 
 namespace DAO
 {
     public class Dummy
     {
 
-        private List<String> lista_Productos;
+        private List<DO_Producto> lista_Productos = new List<DO_Producto>();
 
-        /*("1",750,1000,1,"Leche", 3);
-         * ("2",100,250,1,"Tang", 10);
-         * ("3",500,850,1,"Mantequilla", 5);
-         */
-        public void BuscarProducto (String _codigo)
+        public Dummy()
         {
+            lista_Productos.Add(new DO_Producto("1", 750, 1000, 1, "Leche", 3));
+            lista_Productos.Add(new DO_Producto("2", 100, 250, 1, "Tang", 10));
+            lista_Productos.Add(new DO_Producto("3", 500, 850, 1, "Mantequilla", 5));
+        }
 
+
+        public DO_Producto BuscarProducto (String _codigo)
+        {
+            foreach(DO_Producto producto in lista_Productos)
+            {
+                if (producto.codigo == _codigo)
+                {
+                    return producto;
+                }
+            }
+
+            return new DO_Producto();
         }
 
 
