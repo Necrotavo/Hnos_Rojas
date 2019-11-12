e410600d2fbbe57dcfa987030e8e6b39e070abfa
Daniel Ruiz <ruizaraya14@gmail.com>
2019-11-12 11:35:10 -0600

    Cree DO_FACTURA, DO_CLIENTE, DO_CREDITO

---
+++ ClassLibrary1/DO_Credito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    class DO_Credito
+    {
+        private int monto;
+        private String limiteCredito;
+        private List<DO_Factura> listaFactura;
+    }
+}
