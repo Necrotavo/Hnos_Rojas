f625e505d35d24980720f0b7f34bb56b74cd1b06
KevinJag21 <kevin.araya@ucrso.info>
2019-11-13 11:33:17 -0600

    DAO factura

---
+++ DAO/DAO_Factura.cs
@@ -0,0 +1,40 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DAO
+{
+    public class DAO_Factura
+    {
+        private SqlConnection conexion = new SqlConnection(DAO.Properties.Settings.Default.connectionString);
+
+        public bool guardarFactura(DO_Factura factura) {
+
+            SqlCommand insert = new SqlCommand("insert into FACTURA (FAC_CODIGO, FAC_NOTAS, FAC_CLIENTE, FAC_FECHA,+"
+               +" PLANT_CODIGO, USR_NOMBRE, CRE_IDENTIFICADOR, EST_ESTADO, TP_TIPO) values"+
+               "(@codigo, @notas, @cliente, @fecha, @usrNombre, @credito, @estado, @tipo)", conexion);
+            //falta agregar los paramemtros
+            try {
+                if (conexion.State != ConnectionState.Open) {
+                    conexion.Open();
+                }
+                insert.ExecuteNonQuery();
+                return true;
+            } catch (SqlException) {
+            } finally {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
