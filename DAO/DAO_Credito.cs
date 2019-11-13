c5723bbcb224a0610c2ceec50ec69f2b76021e7a
Daniel Ruiz <ruizaraya14@gmail.com>
2019-11-12 22:37:12 -0600

    nothing

---
+++ DAO/DAO_Credito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DO;
+
+namespace DAO
+{
+    public class DAO_Credito
+    {
+        private SqlConnection conexion = new SqlConnection(DAO.Properties.Settings.Default.connectionString);
+        
+
+            //SqlCommand comando = new SqlCommand("insert into CREDITO (CRED_LIMITE_CREDITO) Values (@limiteCredito)", conexion);
+
+            
+    }
+    }
+}
