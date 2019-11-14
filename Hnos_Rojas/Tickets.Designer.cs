19de5d16321d867ec66583b83d38e8d64ec6beb8
Daniel Ruiz <ruizaraya14@gmail.com>
2019-11-13 22:42:13 -0600

    my commit

--- Hnos_Rojas/Tickets.Designer.cs
+++ Hnos_Rojas/Tickets.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.gridProductos = new System.Windows.Forms.DataGridView();
             this.lblTotal = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCobrar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.rdoContado = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
@@ -42,8 +42,9 @@
             this.gridProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
             this.gridProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridProductos.Location = new System.Drawing.Point(-1, 0);
+            this.gridProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gridProductos.Name = "gridProductos";
-            this.gridProductos.Size = new System.Drawing.Size(770, 251);
+            this.gridProductos.Size = new System.Drawing.Size(1027, 309);
             this.gridProductos.TabIndex = 0;
             this.gridProductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridProductos_CellClick);
             // 
@@ -51,26 +52,28 @@
             // 
             this.lblTotal.AutoSize = true;
             this.lblTotal.Font = new System.Drawing.Font("Segoe UI Historic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(775, 0);
+            this.lblTotal.Location = new System.Drawing.Point(1033, 0);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(209, 86);
+            this.lblTotal.Size = new System.Drawing.Size(255, 106);
             this.lblTotal.TabIndex = 1;
             this.lblTotal.Text = "₡0,00";
             // 
-            // button1
+            // btnCobrar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Historic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(775, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(289, 82);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "F12 Cobrar";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCobrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnCobrar.FlatAppearance.BorderSize = 0;
+            this.btnCobrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCobrar.Font = new System.Drawing.Font("Segoe UI Historic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCobrar.ForeColor = System.Drawing.Color.White;
+            this.btnCobrar.Location = new System.Drawing.Point(1033, 110);
+            this.btnCobrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCobrar.Name = "btnCobrar";
+            this.btnCobrar.Size = new System.Drawing.Size(385, 101);
+            this.btnCobrar.TabIndex = 2;
+            this.btnCobrar.Text = "F12 Cobrar";
+            this.btnCobrar.UseVisualStyleBackColor = false;
+            this.btnCobrar.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -79,9 +82,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI Historic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(-1, 257);
+            this.button2.Location = new System.Drawing.Point(-1, 316);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 30);
+            this.button2.Size = new System.Drawing.Size(200, 37);
             this.button2.TabIndex = 3;
             this.button2.Text = "DEL Borrar Producto";
             this.button2.UseVisualStyleBackColor = false;
@@ -92,9 +96,10 @@
             this.rdoContado.AutoSize = true;
             this.rdoContado.Checked = true;
             this.rdoContado.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoContado.Location = new System.Drawing.Point(775, 177);
+            this.rdoContado.Location = new System.Drawing.Point(1033, 218);
+            this.rdoContado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdoContado.Name = "rdoContado";
-            this.rdoContado.Size = new System.Drawing.Size(157, 41);
+            this.rdoContado.Size = new System.Drawing.Size(193, 50);
             this.rdoContado.TabIndex = 4;
             this.rdoContado.TabStop = true;
             this.rdoContado.Text = "Contado";
@@ -104,25 +109,27 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(775, 210);
+            this.radioButton2.Location = new System.Drawing.Point(1033, 258);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(138, 41);
+            this.radioButton2.Size = new System.Drawing.Size(170, 50);
             this.radioButton2.TabIndex = 5;
             this.radioButton2.Text = "Crédito";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Tickets
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1087, 322);
+            this.ClientSize = new System.Drawing.Size(1449, 396);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.rdoContado);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCobrar);
             this.Controls.Add(this.lblTotal);
             this.Controls.Add(this.gridProductos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Tickets";
             this.Text = "Ticket";
             ((System.ComponentModel.ISupportInitialize)(this.gridProductos)).EndInit();
@@ -135,7 +142,7 @@
 
         private System.Windows.Forms.DataGridView gridProductos;
         private System.Windows.Forms.Label lblTotal;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCobrar;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RadioButton rdoContado;
         private System.Windows.Forms.RadioButton radioButton2;
