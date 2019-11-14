afb8a1828f9ec15da3a8139afb40975b49d5445a
JoJoLa <jonathan.arroyo@ucrso.info>
2019-11-13 22:11:56 -0600

    DAO: Agregado DAO_Usuario. BL: Agregado BL_Usuario.

---
+++ BL/BL_Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+using DAO;
+
+
+namespace BL
+{
+    public class BL_Usuario
+    {
+        /// <summary>
+        /// Carga los usuarios registrados
+        /// </summary>
+        /// <returns>Lista con los usuarios registrados (List<DO_Usuario>)</returns>
+        public List<DO_Usuario> CargarUsuarios()
+        {
+            DAO_Usuario daoUsuario = new DAO_Usuario();
+
+            return daoUsuario.CargarUsuarios();
+        }
+    }
+}
---
+++ DAO/DAO_Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DO;
+
+namespace DAO
+{
+    public class DAO_Usuario
+    {
+        SqlConnection conexion = new SqlConnection(DAO.Properties.Settings.Default.connectionString);
+
+        /// <summary>
+        /// Carga los usuarios registrados en el sistema
+        /// </summary>
+        /// <returns>Lista con los usuarios registrados (List<DO_Usuario>)</returns>
+        public List<DO_Usuario> CargarUsuarios()
+        {
+            SqlCommand consulta = new SqlCommand("select * from Usuario", conexion);
+
+
+            List<DO_Usuario> listaUsuarios = new List<DO_Usuario>();
+
+            try
+            {
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.Open();
+                }
+                SqlDataReader lector = consulta.ExecuteReader();
+                if (lector.HasRows)
+                {
+                    while (lector.Read())
+                    {
+                        listaUsuarios.Add(new DO_Usuario((String)lector["USR_NOMBRE"], (String)lector["TP_TIPO"], (String)lector["USR_CONTRASENA"]));
+                           
+                    }
+                }
+                return listaUsuarios;
+
+            }
+            catch (SqlException)
+            {
+
+            }
+            finally
+            {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+            }
+            return null;
+        }
+    }
+}
