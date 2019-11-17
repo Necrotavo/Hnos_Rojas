d0f66b0ee9dce1350f0ec128c5a14dcf17f67518
JoJoLa <jonathan.arroyo@ucrso.info>
2019-11-17 12:16:28 -0600

    DAO_Asociado: se agregaron los primeros métodos.

--- ClassLibrary1/DO_Asociado.cs
+++ ClassLibrary1/DO_Asociado.cs
@@ -8,19 +8,23 @@ namespace DO
 {
     public class DO_Asociado
     {
-        public int telefono { set; get; }
+        public String identificador { set; get;}
+        public String telefono { set; get; }
         public String primerApellido { set; get; }
         public String segundoApellido { set; get; }
         public String nombre { set; get; }
+        public String nombre_Proveedor { set; get; }
 
         public DO_Asociado() {
         }
 
-        public DO_Asociado(int _telefono, String _primerApellido, String _segundoApellido, String _nombre) {
+        public DO_Asociado(String _identificador,String _telefono, String _primerApellido, String _segundoApellido, String _nombre, String _nombreProveedor) {
+            this.identificador = _identificador;
             this.telefono = _telefono;
             this.primerApellido = _primerApellido;
             this.segundoApellido = _segundoApellido;
             this.nombre = _nombre;
+            this.nombre_Proveedor = _nombreProveedor;
         }
     }
 }
