4ebae727c37ef4eb9e91f950c5b1cbb8c4b34eb9
KevinJag21 <kevin.araya@ucrso.info>
2019-11-17 12:36:16 -0600

    rename a los componentes de las ventanas

--- Hnos_Rojas/LogIn.cs
+++ Hnos_Rojas/LogIn.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BL;
 
 namespace Hnos_Rojas
 {
     public partial class VentanaPrincipal : Form
     {
+        private String usuarioSeleccionado = "";
         public VentanaPrincipal()
         {
             InitializeComponent();
+            llenarCboUsuario();
         }
 
         private void VentanaPrincipal_Load(object sender, EventArgs e)
@@ -32,11 +35,28 @@ namespace Hnos_Rojas
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+   
+
+        private void btnAceptar_Click(object sender, EventArgs e)
         {
+            //BL_Usuario blUsuario = new BL_Usuario();
+            
             Principal principal = new Principal(this);
             principal.Show();
             this.Hide();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void llenarCboUsuario() {
+            BL_Usuario blUsuario = new BL_Usuario();
+            cboUsuario.DataSource = blUsuario.CargarUsuarios();
+            cboUsuario.ValueMember = "nombreUsuario";
+            cboUsuario.DisplayMember = "nombreUsuario";
+            usuarioSeleccionado = cboUsuario.SelectedValue.ToString();
         }
     }
 }
