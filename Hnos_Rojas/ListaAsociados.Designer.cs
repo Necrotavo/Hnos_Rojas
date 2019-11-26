04fda86f290a1b2295377e44e5605ab77a08b506
Daniel Ruiz <ruizaraya14@gmail.com>
2019-11-26 11:58:17 -0600

    Muestra la lista de asociados

--- Hnos_Rojas/ListaAsociados.Designer.cs
+++ Hnos_Rojas/ListaAsociados.Designer.cs
@@ -28,12 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grdAsociados = new System.Windows.Forms.DataGridView();
+            this.btnAddAsociado = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.dataGridViewImageColumn2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Abonar = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grdAsociados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdAsociados
+            // 
+            this.grdAsociados.AllowUserToAddRows = false;
+            this.grdAsociados.AllowUserToDeleteRows = false;
+            this.grdAsociados.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdAsociados.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grdAsociados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdAsociados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Fecha,
+            this.Usuario,
+            this.Editar,
+            this.Abonar});
+            this.grdAsociados.Location = new System.Drawing.Point(409, 120);
+            this.grdAsociados.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grdAsociados.Name = "grdAsociados";
+            this.grdAsociados.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdAsociados.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.grdAsociados.RowTemplate.Height = 24;
+            this.grdAsociados.Size = new System.Drawing.Size(767, 445);
+            this.grdAsociados.TabIndex = 6;
+            // 
+            // btnAddAsociado
+            // 
+            this.btnAddAsociado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(107)))), ((int)(((byte)(94)))));
+            this.btnAddAsociado.FlatAppearance.BorderSize = 0;
+            this.btnAddAsociado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddAsociado.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAddAsociado.ForeColor = System.Drawing.Color.White;
+            this.btnAddAsociado.Location = new System.Drawing.Point(635, 597);
+            this.btnAddAsociado.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddAsociado.Name = "btnAddAsociado";
+            this.btnAddAsociado.Size = new System.Drawing.Size(305, 59);
+            this.btnAddAsociado.TabIndex = 21;
+            this.btnAddAsociado.Text = "Agregar Asociado";
+            this.btnAddAsociado.UseVisualStyleBackColor = false;
+            this.btnAddAsociado.Click += new System.EventHandler(this.btnAddAsociado_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTitulo.Location = new System.Drawing.Point(585, 45);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(204, 36);
+            this.lblTitulo.TabIndex = 22;
+            this.lblTitulo.Text = "Asociados de ";
+            // 
+            // Codigo
+            // 
+            this.Codigo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Codigo.DataPropertyName = "Identificador";
+            this.Codigo.HeaderText = "ID";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.ReadOnly = true;
+            this.Codigo.Width = 65;
+            // 
+            // Fecha
+            // 
+            this.Fecha.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Fecha.DataPropertyName = "Nombre";
+            this.Fecha.HeaderText = "Nombre";
+            this.Fecha.Name = "Fecha";
+            this.Fecha.ReadOnly = true;
+            this.Fecha.Width = 130;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Usuario.DataPropertyName = "Telefono";
+            this.Usuario.HeaderText = "Teléfono";
+            this.Usuario.Name = "Usuario";
+            this.Usuario.ReadOnly = true;
+            this.Usuario.Width = 139;
+            // 
+            // dataGridViewImageColumn1
+            // 
+            this.dataGridViewImageColumn1.HeaderText = "Editar";
+            this.dataGridViewImageColumn1.Image = global::Hnos_Rojas.Properties.Resources.output_onlinepngtools;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            // 
+            // dataGridViewImageColumn2
+            // 
+            this.dataGridViewImageColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dataGridViewImageColumn2.DataPropertyName = "Eliminar";
+            this.dataGridViewImageColumn2.HeaderText = "Eliminar";
+            this.dataGridViewImageColumn2.Image = global::Hnos_Rojas.Properties.Resources.Delete__1_;
+            this.dataGridViewImageColumn2.Name = "dataGridViewImageColumn2";
+            this.dataGridViewImageColumn2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewImageColumn2.Width = 107;
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Image = global::Hnos_Rojas.Properties.Resources.output_onlinepngtools;
+            this.Editar.Name = "Editar";
+            this.Editar.ReadOnly = true;
+            // 
+            // Abonar
+            // 
+            this.Abonar.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Abonar.DataPropertyName = "Eliminar";
+            this.Abonar.HeaderText = "Eliminar";
+            this.Abonar.Image = global::Hnos_Rojas.Properties.Resources.Delete__1_;
+            this.Abonar.Name = "Abonar";
+            this.Abonar.ReadOnly = true;
+            this.Abonar.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Abonar.Width = 107;
+            // 
+            // ListaAsociados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(1673, 684);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnAddAsociado);
+            this.Controls.Add(this.grdAsociados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ListaAsociados";
             this.Text = "ListaAsociados";
+            ((System.ComponentModel.ISupportInitialize)(this.grdAsociados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView grdAsociados;
+        private System.Windows.Forms.Button btnAddAsociado;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewImageColumn Editar;
+        private System.Windows.Forms.DataGridViewImageColumn Abonar;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn2;
     }
 }
--- Hnos_Rojas/ListaAsociados.cs
+++ Hnos_Rojas/ListaAsociados.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BL;
+using DO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,41 @@ namespace Hnos_Rojas
 {
     public partial class ListaAsociados : Form
     {
-        public ListaAsociados()
+        String proveedor;
+        public ListaAsociados(String _proveedor)
         {
             InitializeComponent();
+            proveedor = _proveedor;
+            lblTitulo.Text = lblTitulo.Text + _proveedor;
+            llenarListBox();
+        }
+
+        private void llenarListBox()
+        {
+            BL_Asociado blAsociado = new BL_Asociado();
+
+            List<DO_Asociado> listaProveedores = blAsociado.obtenerListaAsociados(proveedor);
+
+            DataTable tablaAsociados = new DataTable();
+            tablaAsociados.Columns.Add("Identificador");
+            tablaAsociados.Columns.Add("Nombre");
+            tablaAsociados.Columns.Add("Telefono");
+            if (listaProveedores != null)
+            {
+                foreach (DO_Asociado doAsociado in listaProveedores)
+                {
+                    tablaAsociados.Rows.Add(doAsociado.perIdentificador,
+                        doAsociado.perNombre + " " + doAsociado.perPrimerApellido + " " + doAsociado.perSegundoApellido,
+                        doAsociado.perTelefono);
+                }
+            }
+
+            this.grdAsociados.DataSource = tablaAsociados;
+        }
+
+        private void btnAddAsociado_Click(object sender, EventArgs e)
+        {
+            //MostrarFormulario de agregar asociado
         }
     }
 }
