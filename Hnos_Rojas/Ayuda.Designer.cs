1f5b18fdef09bfd88c107e88bdd5872a21452c7d
AndreyCrandys <andrey.nunez@ucrso.info>
2019-11-28 00:32:12 -0600

    Parte de ayuda pero sin texto

--- Hnos_Rojas/Ayuda.Designer.cs
+++ Hnos_Rojas/Ayuda.Designer.cs
@@ -28,11 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAyudaVentas = new System.Windows.Forms.Button();
+            this.btnAyudaClientes = new System.Windows.Forms.Button();
+            this.btnAyudaProductos = new System.Windows.Forms.Button();
+            this.btnAyudaProveedores = new System.Windows.Forms.Button();
+            this.btnAyudaConfiguracion = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.txtAyuda = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAyudaVentas
+            // 
+            this.btnAyudaVentas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyudaVentas.FlatAppearance.BorderSize = 0;
+            this.btnAyudaVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyudaVentas.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAyudaVentas.ForeColor = System.Drawing.Color.White;
+            this.btnAyudaVentas.Location = new System.Drawing.Point(12, 12);
+            this.btnAyudaVentas.Name = "btnAyudaVentas";
+            this.btnAyudaVentas.Size = new System.Drawing.Size(166, 88);
+            this.btnAyudaVentas.TabIndex = 9;
+            this.btnAyudaVentas.Text = "Ventas";
+            this.btnAyudaVentas.UseVisualStyleBackColor = false;
+            this.btnAyudaVentas.Click += new System.EventHandler(this.btnAyudaVentas_Click);
+            // 
+            // btnAyudaClientes
+            // 
+            this.btnAyudaClientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyudaClientes.FlatAppearance.BorderSize = 0;
+            this.btnAyudaClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyudaClientes.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAyudaClientes.ForeColor = System.Drawing.Color.White;
+            this.btnAyudaClientes.Location = new System.Drawing.Point(184, 12);
+            this.btnAyudaClientes.Name = "btnAyudaClientes";
+            this.btnAyudaClientes.Size = new System.Drawing.Size(166, 88);
+            this.btnAyudaClientes.TabIndex = 10;
+            this.btnAyudaClientes.Text = "Clientes";
+            this.btnAyudaClientes.UseVisualStyleBackColor = false;
+            this.btnAyudaClientes.Click += new System.EventHandler(this.btnAyudaClientes_Click);
+            // 
+            // btnAyudaProductos
+            // 
+            this.btnAyudaProductos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyudaProductos.FlatAppearance.BorderSize = 0;
+            this.btnAyudaProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyudaProductos.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAyudaProductos.ForeColor = System.Drawing.Color.White;
+            this.btnAyudaProductos.Location = new System.Drawing.Point(356, 12);
+            this.btnAyudaProductos.Name = "btnAyudaProductos";
+            this.btnAyudaProductos.Size = new System.Drawing.Size(166, 88);
+            this.btnAyudaProductos.TabIndex = 11;
+            this.btnAyudaProductos.Text = "Productos";
+            this.btnAyudaProductos.UseVisualStyleBackColor = false;
+            this.btnAyudaProductos.Click += new System.EventHandler(this.btnAyudaProductos_Click);
+            // 
+            // btnAyudaProveedores
+            // 
+            this.btnAyudaProveedores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyudaProveedores.FlatAppearance.BorderSize = 0;
+            this.btnAyudaProveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyudaProveedores.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAyudaProveedores.ForeColor = System.Drawing.Color.White;
+            this.btnAyudaProveedores.Location = new System.Drawing.Point(528, 12);
+            this.btnAyudaProveedores.Name = "btnAyudaProveedores";
+            this.btnAyudaProveedores.Size = new System.Drawing.Size(166, 88);
+            this.btnAyudaProveedores.TabIndex = 12;
+            this.btnAyudaProveedores.Text = "Proveedores";
+            this.btnAyudaProveedores.UseVisualStyleBackColor = false;
+            this.btnAyudaProveedores.Click += new System.EventHandler(this.btnAyudaProveedores_Click);
+            // 
+            // btnAyudaConfiguracion
+            // 
+            this.btnAyudaConfiguracion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyudaConfiguracion.FlatAppearance.BorderSize = 0;
+            this.btnAyudaConfiguracion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyudaConfiguracion.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnAyudaConfiguracion.ForeColor = System.Drawing.Color.White;
+            this.btnAyudaConfiguracion.Location = new System.Drawing.Point(700, 12);
+            this.btnAyudaConfiguracion.Name = "btnAyudaConfiguracion";
+            this.btnAyudaConfiguracion.Size = new System.Drawing.Size(178, 88);
+            this.btnAyudaConfiguracion.TabIndex = 13;
+            this.btnAyudaConfiguracion.Text = "Configuración";
+            this.btnAyudaConfiguracion.UseVisualStyleBackColor = false;
+            this.btnAyudaConfiguracion.Click += new System.EventHandler(this.btnAyudaConfiguracion_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Segoe UI Historic", 18F);
+            this.btnReportes.ForeColor = System.Drawing.Color.White;
+            this.btnReportes.Location = new System.Drawing.Point(884, 12);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(166, 88);
+            this.btnReportes.TabIndex = 14;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = false;
+            this.btnReportes.Click += new System.EventHandler(this.btnReportes_Click);
+            // 
+            // txtAyuda
+            // 
+            this.txtAyuda.Location = new System.Drawing.Point(12, 116);
+            this.txtAyuda.Multiline = true;
+            this.txtAyuda.Name = "txtAyuda";
+            this.txtAyuda.ReadOnly = true;
+            this.txtAyuda.Size = new System.Drawing.Size(1038, 514);
+            this.txtAyuda.TabIndex = 15;
+            // 
+            // Ayuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(107)))), ((int)(((byte)(94)))));
+            this.ClientSize = new System.Drawing.Size(1301, 661);
+            this.Controls.Add(this.txtAyuda);
+            this.Controls.Add(this.btnReportes);
+            this.Controls.Add(this.btnAyudaConfiguracion);
+            this.Controls.Add(this.btnAyudaProveedores);
+            this.Controls.Add(this.btnAyudaProductos);
+            this.Controls.Add(this.btnAyudaClientes);
+            this.Controls.Add(this.btnAyudaVentas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Ayuda";
             this.Text = "Ayuda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAyudaVentas;
+        private System.Windows.Forms.Button btnAyudaClientes;
+        private System.Windows.Forms.Button btnAyudaProductos;
+        private System.Windows.Forms.Button btnAyudaProveedores;
+        private System.Windows.Forms.Button btnAyudaConfiguracion;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.TextBox txtAyuda;
     }
 }
